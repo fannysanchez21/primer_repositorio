adb11b096e919e0bc37e22e995f75cfd70b10f46
fannysanchez21 <a385866@uach.mx>
2025-10-01 08:18:43 -0700

    Create progama.py

---
+++ progama.py
@@ -0,0 +1,6 @@
+# Este es un script de python de prueba para el repositorio
+def saludo(nombre):
+    return "Hola" + nombre
+
+# Se invoca la funcion saludo para saludar a alguien llamado fanny
+saludo("fanny")
